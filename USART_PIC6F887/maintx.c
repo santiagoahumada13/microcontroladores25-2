dea4779f6776c77c164e9d9e55a9d801564662b7
santiagoahumada13 <124843945+santiagoahumada13@users.noreply.github.com>
2025-05-17 19:44:02 -0600

    Add files via upload

---
+++ USART_PIC6F887/maintx.c
@@ -0,0 +1,57 @@
+/*
+    Santiago Ahumada Sierra 
+    Comunicacion serial PIC16F887 Transmisor
+    Microcontroladores 2809
+    M. en C. Jorge Ricardo Gersenowies Rosas
+    Semestre 2025-II
+    Facultad de Estudios Superiores Cuautitlan
+    Universidad Nacional Autonoma de Mexico
+
+*/
+
+// CONFIG1
+#pragma config FOSC = INTRC_NOCLKOUT// Oscillator Selection bits (INTOSCIO oscillator: I/O function on RA6/OSC2/CLKOUT pin, I/O function on RA7/OSC1/CLKIN)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled and can be enabled by SWDTEN bit of the WDTCON register)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT disabled)
+#pragma config MCLRE = ON       // RE3/MCLR pin function select bit (RE3/MCLR pin function is MCLR)
+#pragma config CP = OFF         // Code Protection bit (Program memory code protection is disabled)
+#pragma config CPD = OFF        // Data Code Protection bit (Data memory code protection is disabled)
+#pragma config BOREN = ON       // Brown Out Reset Selection bits (BOR enabled)
+#pragma config IESO = ON        // Internal External Switchover bit (Internal/External Switchover mode is enabled)
+#pragma config FCMEN = ON       // Fail-Safe Clock Monitor Enabled bit (Fail-Safe Clock Monitor is enabled)
+#pragma config LVP = ON         // Low Voltage Programming Enable bit (RB3/PGM pin has PGM function, low voltage programming enabled)
+
+// CONFIG2
+#pragma config BOR4V = BOR40V   // Brown-out Reset Selection bit (Brown-out Reset set to 4.0V)
+#pragma config WRT = OFF        // Flash Program Memory Self Write Enable bits (Write protection off)
+
+#include <xc.h>
+#define _XTAL_FREQ 4000000//Declare internal OSC Freq as 4MHz
+
+void USARTWriteChr(unsigned char c){
+    while(PIR1bits.TXIF==0);
+    TXREG=c;
+}
+void main(){
+    ANSELH=0;
+    TRISB=0xFF;
+    WPUB=0xFF;
+    
+    PIE1bits.TXIE=1;
+    INTCONbits.GIE=1;
+    INTCONbits.PEIE=1;
+    
+    RCSTAbits.SPEN=1;
+    TXSTAbits.TXEN=1;
+    TXSTAbits.SYNC=0;
+    BAUDCTLbits.BRG16=0;
+    TXSTAbits.BRGH=1;
+    
+    
+    SPBRG=25;
+    
+    while(1){
+        USARTWriteChr(PORTB);
+        __delay_ms(100);
+    }
+}
